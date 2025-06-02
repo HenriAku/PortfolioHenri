64e909b4f20a910c47f18ab81400693d2f9e33a9
Henri <rougeollehenri@gmail.com>
2025-06-02 14:47:22 +0200

    modif url contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -17,7 +17,7 @@ const handleSubmit = async () => {
   }
 
   try {
-    const response = await fetch('http://localhost:3001/api/contact', {
+    const response = await fetch('https://portfoliohenri-backend.onrender.com/api/contact', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(form)
